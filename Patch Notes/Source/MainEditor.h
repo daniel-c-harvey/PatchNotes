a63c7b9f8afd41d91899da3911aae05639163a43
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-02-12 19:37:20 -0500

    init

---
+++ Patch Notes/Source/MainEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    MainEditor.h
+    Created: 12 Feb 2024 5:57:27pm
+    Author:  dharvey
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PatchNotesMainEditor : public juce::AudioProcessorEditor
+{
+public:
+    PatchNotesMainEditor(PatchNotesMainEditor&);
+    ~PatchNotesMainEditor() override;
+
+    //==============================================================================
+    void paint(juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PatchNotesMainProcessor& midiProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PatchNotesMainEditor)
+};
